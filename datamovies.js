b3b1af94b3f390b58130499735de68f4d15fe7ba
Deepak Pandey <deepak8574757609@gmail.com>
2022-12-16 21:27:02 +0530

    all page done

---
+++ datamovies.js
@@ -0,0 +1,93 @@
+moviesdata= [
+
+    {
+        img_url:"https://akamaividz2.zee5.com/image/upload/w_224,h_336,c_scale,f_webp,q_auto:eco/resources/0-0-1z5259649/portrait/1920x770c85c702415cb46fdbc2b704334a1222d.jpg",
+        name:"Blurr",
+        cat: "Action"
+        
+    },
+    {
+        img_url:"https://akamaividz2.zee5.com/image/upload/w_224,h_336,c_scale,f_webp,q_auto:eco/resources/0-0-1z5259649/portrait/1920x770c85c702415cb46fdbc2b704334a1222d.jpg",
+        name:"Blurr",
+        cat: "Romance"
+    },
+    {
+        img_url:"https://akamaividz2.zee5.com/image/upload/w_224,h_336,c_scale,f_webp,q_auto:eco/resources/0-0-1z5259649/portrait/1920x770c85c702415cb46fdbc2b704334a1222d.jpg",
+        name:"Blurr",
+        cat: "Drama"
+        
+    },
+    {
+        img_url:"https://akamaividz2.zee5.com/image/upload/w_224,h_336,c_scale,f_webp,q_auto:eco/resources/0-0-1z5259649/portrait/1920x770c85c702415cb46fdbc2b704334a1222d.jpg",
+        name:"Blurr",
+        cat: "Horror"
+        
+    },
+    {
+        img_url:"https://akamaividz2.zee5.com/image/upload/w_224,h_336,c_scale,f_webp,q_auto:eco/resources/0-0-1z5259649/portrait/1920x770c85c702415cb46fdbc2b704334a1222d.jpg",
+        name:"Blurr",
+        cat: "Action"
+        
+    },
+
+
+    {
+        img_url:"https://akamaividz2.zee5.com/image/upload/w_224,h_336,c_scale,f_webp,q_auto:eco/resources/0-0-1z5259649/portrait/1920x770c85c702415cb46fdbc2b704334a1222d.jpg",
+        name:"Blurr",
+        cat: "Romance"
+        
+    },
+    {
+        img_url:"https://akamaividz2.zee5.com/image/upload/w_224,h_336,c_scale,f_webp,q_auto:eco/resources/0-0-1z5259649/portrait/1920x770c85c702415cb46fdbc2b704334a1222d.jpg",
+        name:"Blurr",
+        cat: "Horror"
+        
+    },
+    {
+        img_url:"https://akamaividz2.zee5.com/image/upload/w_224,h_336,c_scale,f_webp,q_auto:eco/resources/0-0-1z5259649/portrait/1920x770c85c702415cb46fdbc2b704334a1222d.jpg",
+        name:"Blurr",
+        cat: "Action"
+        
+    },
+    {
+        img_url:"https://akamaividz2.zee5.com/image/upload/w_224,h_336,c_scale,f_webp,q_auto:eco/resources/0-0-1z5259649/portrait/1920x770c85c702415cb46fdbc2b704334a1222d.jpg",
+        name:"Blurr",
+        cat: "Drama"
+        
+    },
+    {
+        img_url:"https://akamaividz2.zee5.com/image/upload/w_224,h_336,c_scale,f_webp,q_auto:eco/resources/0-0-1z5259649/portrait/1920x770c85c702415cb46fdbc2b704334a1222d.jpg",
+        name:"Blurr",
+        cat: "Drama"
+        
+    },
+    {
+        img_url:"https://akamaividz2.zee5.com/image/upload/w_224,h_336,c_scale,f_webp,q_auto:eco/resources/0-0-1z5259649/portrait/1920x770c85c702415cb46fdbc2b704334a1222d.jpg",
+        name:"Blurr",
+        cat: "Action"
+        
+    },
+    {
+        img_url:"https://akamaividz2.zee5.com/image/upload/w_224,h_336,c_scale,f_webp,q_auto:eco/resources/0-0-1z5259649/portrait/1920x770c85c702415cb46fdbc2b704334a1222d.jpg",
+        name:"Blurr",
+        cat: "Horror"
+        
+    },
+
+
+    {
+        img_url:"https://akamaividz2.zee5.com/image/upload/w_224,h_336,c_scale,f_webp,q_auto:eco/resources/0-0-1z5259649/portrait/1920x770c85c702415cb46fdbc2b704334a1222d.jpg",
+        name:"Blurr",
+        cat: "Action"
+        
+    },
+    {
+        img_url:"https://akamaividz2.zee5.com/image/upload/w_224,h_336,c_scale,f_webp,q_auto:eco/resources/0-0-1z5259649/portrait/1920x770c85c702415cb46fdbc2b704334a1222d.jpg",
+        name:"Blurr",
+        cat: "Horror"
+        
+    },
+    
+]
+
+localStorage.setItem("moviemore",JSON.stringify(moviesdata))
