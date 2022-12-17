5a8d644186f659cc827a1453db045baa0ed9b9f3
Deepak Pandey <deepak8574757609@gmail.com>
2022-12-17 19:10:35 +0530

    footer complete and site become responsible

--- datamovies.js
+++ datamovies.js
@@ -1,92 +1,164 @@
 moviesdata= [
 
     {
-        img_url:"https://akamaividz2.zee5.com/image/upload/w_224,h_336,c_scale,f_webp,q_auto:eco/resources/0-0-1z5259649/portrait/1920x770c85c702415cb46fdbc2b704334a1222d.jpg",
-        name:"Blurr",
+        img_url:"https://akamaividz2.zee5.com/image/upload/w_224,h_336,c_scale,f_webp,q_auto:eco/resources/0-0-1z5250012/portrait/1920x770541441bea82b4c5ebb5e50fdf65b1486.jpg",
+        name:"Indian Lockdown",
         cat: "Action"
         
     },
     {
-        img_url:"https://akamaividz2.zee5.com/image/upload/w_224,h_336,c_scale,f_webp,q_auto:eco/resources/0-0-1z5259649/portrait/1920x770c85c702415cb46fdbc2b704334a1222d.jpg",
-        name:"Blurr",
+        img_url:"https://akamaividz2.zee5.com/image/upload/w_231,h_347,c_scale,f_webp,q_auto:eco/resources/0-6-4z5199975/portrait/1920x7700b1f9a5596a848a493f7bd6a3d226928.jpg",
+        name:"Mukhbir",
+        cat: "Action"
+    },
+    {
+        img_url:"https://akamaividz2.zee5.com/image/upload/w_231,h_347,c_scale,f_webp,q_auto:eco/resources/0-0-1z5254708/portrait/1920x77088754fa6d1384d259444f7d1d4b00a33.jpg",
+        name:"Chup",
+        cat: "Drama"
+        
+    },
+    {
+        img_url:"https://akamaividz2.zee5.com/image/upload/w_231,h_347,c_scale,f_webp,q_auto:eco/resources/0-0-1z5228525/portrait/1920x77069dfde7f955d4ef69e73c3b682aa636b.jpg",
+        name:"Raksha Bandhan",
+        cat: "Horror"
+        
+    },
+    {
+        img_url:"https://akamaividz2.zee5.com/image/upload/w_231,h_347,c_scale,f_webp,q_auto:eco/resources/0-0-1z5250530/portrait/1920x770129e19e854434fa98ff8734cfc2d6d32670f87db468e4c03bd9511d3b0347094.jpg",
+        name:"Bimbisara",
+        cat: "Action"
+        
+    },
+
+
+    {
+        img_url:"https://akamaividz2.zee5.com/image/upload/w_231,h_347,c_scale,f_webp,q_auto:eco/resources/0-0-1z5266343/portrait/1920x770adfe1ea302d644f69d29804dae8b13b1.jpg",
+        name:"Hansan",
         cat: "Romance"
+        
     },
     {
-        img_url:"https://akamaividz2.zee5.com/image/upload/w_224,h_336,c_scale,f_webp,q_auto:eco/resources/0-0-1z5259649/portrait/1920x770c85c702415cb46fdbc2b704334a1222d.jpg",
+        img_url:"https://akamaividz2.zee5.com/image/upload/w_231,h_347,c_scale,f_webp,q_auto:eco/resources/0-0-1z5266165/portrait/1920x770e10edd19f43d40098d3a1d7c5ba08ef1.jpg",
+        name:"Beeba Boys",
+        cat: "Drama"
+        
+    },
+    {
+        img_url:"https://akamaividz2.zee5.com/image/upload/w_231,h_347,c_scale,f_webp,q_auto:eco/resources/0-0-1z5259649/portrait/1920x770c85c702415cb46fdbc2b704334a1222d.jpg",
+        name:"Blurr",
+        cat: "Action"
+        
+    },
+    {
+        img_url:"https://akamaividz2.zee5.com/image/upload/w_224,h_336,c_scale,f_webp,q_auto:eco/resources/0-0-1z5262283/portrait/1920x7709a2c3520660648989b17c5188c894be2.jpg",
+        name:"Karthikeya 2",
+        cat: "Drama"
+        
+    },
+    {
+        img_url:"https://akamaividz2.zee5.com/image/upload/w_231,h_347,c_scale,f_webp,q_auto:eco/resources/0-0-1z5259649/portrait/1920x770c85c702415cb46fdbc2b704334a1222d.jpg",
         name:"Blurr",
         cat: "Drama"
         
     },
     {
-        img_url:"https://akamaividz2.zee5.com/image/upload/w_224,h_336,c_scale,f_webp,q_auto:eco/resources/0-0-1z5259649/portrait/1920x770c85c702415cb46fdbc2b704334a1222d.jpg",
-        name:"Blurr",
+        img_url:"https://akamaividz2.zee5.com/image/upload/w_231,h_347,c_scale,f_webp,q_auto:eco/resources/0-0-1z5266008/portrait/1920x770c20c6d9a8c2e4723a4ff1a9be4556424.jpg",
+        name:"Bimbisara",
+        cat: "Action"
+        
+    },
+    {
+        img_url:"https://akamaividz2.zee5.com/image/upload/w_231,h_347,c_scale,f_webp,q_auto:eco/resources/0-0-1z5264517/portrait/1920x770618f9da3dbd54edcac6cd320187bf1f2e6966885630a401fb3ed9fc393b63bbd.jpg",
+        name:"Coffee with kadhal",
         cat: "Horror"
         
     },
     {
-        img_url:"https://akamaividz2.zee5.com/image/upload/w_224,h_336,c_scale,f_webp,q_auto:eco/resources/0-0-1z5259649/portrait/1920x770c85c702415cb46fdbc2b704334a1222d.jpg",
-        name:"Blurr",
+        img_url:"https://akamaividz2.zee5.com/image/upload/w_224,h_336,c_scale,f_webp,q_auto:eco/resources/0-0-1z5250012/portrait/1920x770541441bea82b4c5ebb5e50fdf65b1486.jpg",
+        name:"Har Har Mahadev",
+        cat: "Action"
+        
+    },
+    {
+        img_url:"https://akamaividz2.zee5.com/image/upload/w_224,h_336,c_scale,f_webp,q_auto:eco/resources/0-0-1z5250012/portrait/1920x770541441bea82b4c5ebb5e50fdf65b1486.jpg",
+        name:"Indian Lockdown",
+        cat: "Action"
+        
+    },
+    {
+        img_url:"https://akamaividz2.zee5.com/image/upload/w_231,h_347,c_scale,f_webp,q_auto:eco/resources/0-6-4z5199975/portrait/1920x7700b1f9a5596a848a493f7bd6a3d226928.jpg",
+        name:"Mukhbir",
+        cat: "Action"
+    },
+    {
+        img_url:"https://akamaividz2.zee5.com/image/upload/w_231,h_347,c_scale,f_webp,q_auto:eco/resources/0-0-1z5254708/portrait/1920x77088754fa6d1384d259444f7d1d4b00a33.jpg",
+        name:"Chup",
+        cat: "Drama"
+        
+    },
+    {
+        img_url:"https://akamaividz2.zee5.com/image/upload/w_231,h_347,c_scale,f_webp,q_auto:eco/resources/0-0-1z5228525/portrait/1920x77069dfde7f955d4ef69e73c3b682aa636b.jpg",
+        name:"Raksha Bandhan",
+        cat: "Horror"
+        
+    },
+    {
+        img_url:"https://akamaividz2.zee5.com/image/upload/w_231,h_347,c_scale,f_webp,q_auto:eco/resources/0-0-1z5250530/portrait/1920x770129e19e854434fa98ff8734cfc2d6d32670f87db468e4c03bd9511d3b0347094.jpg",
+        name:"Bimbisara",
         cat: "Action"
         
     },
 
 
     {
-        img_url:"https://akamaividz2.zee5.com/image/upload/w_224,h_336,c_scale,f_webp,q_auto:eco/resources/0-0-1z5259649/portrait/1920x770c85c702415cb46fdbc2b704334a1222d.jpg",
-        name:"Blurr",
+        img_url:"https://akamaividz2.zee5.com/image/upload/w_231,h_347,c_scale,f_webp,q_auto:eco/resources/0-0-1z5266343/portrait/1920x770adfe1ea302d644f69d29804dae8b13b1.jpg",
+        name:"Hansan",
         cat: "Romance"
         
     },
     {
-        img_url:"https://akamaividz2.zee5.com/image/upload/w_224,h_336,c_scale,f_webp,q_auto:eco/resources/0-0-1z5259649/portrait/1920x770c85c702415cb46fdbc2b704334a1222d.jpg",
-        name:"Blurr",
-        cat: "Horror"
+        img_url:"https://akamaividz2.zee5.com/image/upload/w_231,h_347,c_scale,f_webp,q_auto:eco/resources/0-0-1z5266165/portrait/1920x770e10edd19f43d40098d3a1d7c5ba08ef1.jpg",
+        name:"Beeba Boys",
+        cat: "Drama"
         
     },
     {
-        img_url:"https://akamaividz2.zee5.com/image/upload/w_224,h_336,c_scale,f_webp,q_auto:eco/resources/0-0-1z5259649/portrait/1920x770c85c702415cb46fdbc2b704334a1222d.jpg",
+        img_url:"https://akamaividz2.zee5.com/image/upload/w_231,h_347,c_scale,f_webp,q_auto:eco/resources/0-0-1z5259649/portrait/1920x770c85c702415cb46fdbc2b704334a1222d.jpg",
         name:"Blurr",
         cat: "Action"
         
     },
     {
-        img_url:"https://akamaividz2.zee5.com/image/upload/w_224,h_336,c_scale,f_webp,q_auto:eco/resources/0-0-1z5259649/portrait/1920x770c85c702415cb46fdbc2b704334a1222d.jpg",
+        img_url:"https://akamaividz2.zee5.com/image/upload/w_224,h_336,c_scale,f_webp,q_auto:eco/resources/0-0-1z5262283/portrait/1920x7709a2c3520660648989b17c5188c894be2.jpg",
+        name:"Karthikeya 2",
+        cat: "Drama"
+        
+    },
+    {
+        img_url:"https://akamaividz2.zee5.com/image/upload/w_231,h_347,c_scale,f_webp,q_auto:eco/resources/0-0-1z5259649/portrait/1920x770c85c702415cb46fdbc2b704334a1222d.jpg",
         name:"Blurr",
         cat: "Drama"
         
     },
     {
-        img_url:"https://akamaividz2.zee5.com/image/upload/w_224,h_336,c_scale,f_webp,q_auto:eco/resources/0-0-1z5259649/portrait/1920x770c85c702415cb46fdbc2b704334a1222d.jpg",
-        name:"Blurr",
-        cat: "Drama"
-        
-    },
-    {
-        img_url:"https://akamaividz2.zee5.com/image/upload/w_224,h_336,c_scale,f_webp,q_auto:eco/resources/0-0-1z5259649/portrait/1920x770c85c702415cb46fdbc2b704334a1222d.jpg",
-        name:"Blurr",
+        img_url:"https://akamaividz2.zee5.com/image/upload/w_231,h_347,c_scale,f_webp,q_auto:eco/resources/0-0-1z5266008/portrait/1920x770c20c6d9a8c2e4723a4ff1a9be4556424.jpg",
+        name:"Bimbisara",
         cat: "Action"
         
     },
     {
-        img_url:"https://akamaividz2.zee5.com/image/upload/w_224,h_336,c_scale,f_webp,q_auto:eco/resources/0-0-1z5259649/portrait/1920x770c85c702415cb46fdbc2b704334a1222d.jpg",
-        name:"Blurr",
+        img_url:"https://akamaividz2.zee5.com/image/upload/w_231,h_347,c_scale,f_webp,q_auto:eco/resources/0-0-1z5264517/portrait/1920x770618f9da3dbd54edcac6cd320187bf1f2e6966885630a401fb3ed9fc393b63bbd.jpg",
+        name:"Coffee with kadhal",
         cat: "Horror"
         
     },
-
-
     {
-        img_url:"https://akamaividz2.zee5.com/image/upload/w_224,h_336,c_scale,f_webp,q_auto:eco/resources/0-0-1z5259649/portrait/1920x770c85c702415cb46fdbc2b704334a1222d.jpg",
-        name:"Blurr",
+        img_url:"https://akamaividz2.zee5.com/image/upload/w_224,h_336,c_scale,f_webp,q_auto:eco/resources/0-0-1z5250012/portrait/1920x770541441bea82b4c5ebb5e50fdf65b1486.jpg",
+        name:"Har Har Mahadev",
         cat: "Action"
         
     },
-    {
-        img_url:"https://akamaividz2.zee5.com/image/upload/w_224,h_336,c_scale,f_webp,q_auto:eco/resources/0-0-1z5259649/portrait/1920x770c85c702415cb46fdbc2b704334a1222d.jpg",
-        name:"Blurr",
-        cat: "Horror"
-        
-    },
+
     
 ]
 
